75a6d692a9891ab58eb26524076ab3c781eb2460
Muntaser Syed <jemsbhai@gmail.com>
2020-07-27 02:37:42 -0400

    Create addasadaqahrequest.py

---
+++ backend/addasadaqahrequest.py
@@ -0,0 +1,71 @@
+import os
+import pymongo
+import json
+
+def dummy(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    if request.method == 'OPTIONS':
+        # Allows GET requests from origin https://mydomain.com with
+        # Authorization header
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Max-Age': '3600',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return ('', 204, headers)
+
+    # Set CORS headers for main requests
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    }
+
+    request_json = request.get_json()
+    mongostr = os.environ.get('MONGOSTR')
+    client = pymongo.MongoClient(mongostr)
+    db = client["sadaqah"]
+    col = db.sadaqah
+    results = []
+    maxid = 0
+    for x in col.find():
+        id = x["id"]
+        maxid +=1
+    id = str(maxid+1)
+    payload = {}
+    # {"phone":"123456","name":"Abdallah","description":"Can't afford medication", "email":"someone@someone.com"}
+    if request_json:
+        payload["id"] = id
+        payload["name"] = request_json['name']
+        payload["phone"] = request_json['phone']
+        payload["email"] = request_json['email']
+        payload["description"] = request_json['description']
+        payload['status'] = "open"
+        
+        result=col.insert_one(payload)
+
+        retjson = {}
+
+        # retjson['dish'] = userid
+        retjson['mongoresult'] = "successfully added"
+        retjson['id'] = id
+
+        return json.dumps(retjson)
+
+
+    retstr = "action not done"
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return retstr
